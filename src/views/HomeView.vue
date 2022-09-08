ff350aee0b932dc911cd2ba0d6ebbbff793b63a8
Camila <criscamila09@gmail.com>
2022-09-08 10:34:55 -0300

    banner

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,18 @@
 <template>
+  <Banner />
   <div class="main-container">
-    <h1>Home</h1>
+    <h1>Make you burguer:</h1>
+    <p>form</p>
   </div>
 </template>
 
-<script></script>
+<script>
+import Banner from "@/components/Banner.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Banner,
+  },
+};
+</script>
