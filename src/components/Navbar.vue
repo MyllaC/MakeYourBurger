579e57ca0d369c747ba914ada67ee69b17fd57d2
Camila <criscamila09@gmail.com>
2022-09-08 09:47:47 -0300

    implementando o Vue Router

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <ndiv id="nav">
+    <router-link to="/">
+      <img src="" alt="logo" id="logo" />
+    </router-link>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/orders">Orders</router-link>
+  </ndiv>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style scoped></style>
